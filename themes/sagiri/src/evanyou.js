7663488997490d7183b9c609fe3a9f62741bbfd7
EGo14T <974412906@qq.com>
2019-12-10 11:54:48 +0800

    add background

--- themes/sagiri/src/evanyou.js
+++ themes/sagiri/src/evanyou.js
@@ -1,44 +1,94 @@
-if (document.getElementById('evanyou')) {
-  var c = document.getElementById('evanyou'),
-    x = c.getContext('2d'),
-    pr = window.devicePixelRatio || 1,
-    w = window.innerWidth,
-    h = window.innerHeight,
-    f = 90,
-    q,
-    m = Math,
-    r = 0,
-    u = m.PI * 2,
-    v = m.cos,
-    z = m.random
-  c.width = w * pr
-  c.height = h * pr
-  x.scale(pr, pr)
-  x.globalAlpha = 0.6
-  function evanyou () {
-    x.clearRect(0, 0, w, h)
-    q = [{ x: 0, y: h * .7 + f }, { x: 0, y: h * .7 - f }]
-    while (q[1].x < w + f) d(q[0], q[1])
-  }
-  function d (i, j) {
-    x.beginPath()
-    x.moveTo(i.x, i.y)
-    x.lineTo(j.x, j.y)
-    var k = j.x + (z() * 2 - 0.25) * f,
-      n = y(j.y)
-    x.lineTo(k, n)
-    x.closePath()
-    r -= u / -50
-    x.fillStyle = '#' + (v(r) * 127 + 128 << 16 | v(r + u / 3) * 127 + 128 << 8 | v(r + u / 3 * 2) * 127 + 128).toString(16)
-    x.fill()
-    q[0] = q[1]
-    q[1] = { x: k, y: n }
-  }
-  function y (p) {
-    var t = p + (z() * 2 - 1.1) * f
-    return (t > h || t < 0) ? y(p) : t
-  }
-  document.onclick = evanyou
-  document.ontouchstart = evanyou
-  evanyou()
-}
+!function() {
+    function o(w, v, i) {
+        return w.getAttribute(v) || i
+    }
+    function j(i) {
+        return document.getElementsByTagName(i)
+    }
+    function l() {
+        var i = j("script"),
+        w = i.length,
+        v = i[w - 1];
+        return {
+            l: w,
+            z: o(v, "zIndex", -1),
+            o: o(v, "opacity", 0.5),
+            c: o(v, "color", "0,0,0"),
+            n: o(v, "count", 99)
+        }
+    }
+    function k() {
+        r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+        n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    }
+    function b() {
+        e.clearRect(0, 0, r, n);
+        var w = [f].concat(t);
+        var x, v, A, B, z, y;
+        t.forEach(function(i) {
+            i.x += i.xa,
+            i.y += i.ya,
+            i.xa *= i.x > r || i.x < 0 ? -1 : 1,
+            i.ya *= i.y > n || i.y < 0 ? -1 : 1,
+            e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
+            for (v = 0; v < w.length; v++) {
+                x = w[v];
+                if (i !== x && null !== x.x && null !== x.y) {
+                    B = i.x - x.x,
+                    z = i.y - x.y,
+                    y = B * B + z * z;
+                    y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke())
+                }
+            }
+            w.splice(w.indexOf(i), 1)
+        }),
+        m(b)
+    }
+    var u = document.getElementById('evanyou'),
+    s = l(),
+    c = "c_n" + s.l,
+    e = u.getContext("2d"),
+    r,
+    n,
+    m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(i) {
+        window.setTimeout(i, 1000 / 45)
+    },
+    a = Math.random,
+    f = {
+        x: null,
+        y: null,
+        max: 20000
+    };
+    u.id = c;
+    u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
+    j("body")[0].appendChild(u);
+    k(),
+    window.onresize = k;
+    window.onmousemove = function(i) {
+        i = i || window.event,
+        f.x = i.clientX,
+        f.y = i.clientY
+    },
+    window.onmouseout = function() {
+        f.x = null,
+        f.y = null
+    };
+    for (var t = [], p = 0; s.n > p; p++) {
+        var h = a() * r,
+        g = a() * n,
+        q = 2 * a() - 1,
+        d = 2 * a() - 1;
+        t.push({
+            x: h,
+            y: g,
+            xa: q,
+            ya: d,
+            max: 6000
+        })
+    }
+    setTimeout(function() {
+        b()
+    },
+    100)
+} ();
